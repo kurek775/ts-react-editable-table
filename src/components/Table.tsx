410bef95c6cf70f5ef71238c121927f92257c622
kurek775 <pavelkurek@seznam.cz>
2024-12-18 16:21:39 +0100

    WIP - init table function

---
+++ src/components/Table.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+type TableProps = {
+  headers: string[];
+  data: Array<Record<string, any>>;
+};
+
+const Table: React.FC<TableProps> = ({ headers, data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {headers.map((header, colIndex) => (
+              <td key={colIndex}>{row[header]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
